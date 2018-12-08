7a374af5db627b66ea50432f16fcb474461beabe
Jenny Eckstein <jenny@Jennys-MacBook-Pro.local>
2018-12-07 20:08:03 -0500

    linter

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const app = express();
 
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('Express Setup');
-})
+  res.send('Express Setup');
+});
